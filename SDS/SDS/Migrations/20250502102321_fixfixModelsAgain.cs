b758becc23f08ff66debd847bf6e2af0bf957701
Eulean <eulen9t5@gmail.com>
2025-05-05 04:26:37 +0630

    feat: add migrations file

---
+++ SDS/SDS/Migrations/20250502102321_fixfixModelsAgain.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SDS.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixfixModelsAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
